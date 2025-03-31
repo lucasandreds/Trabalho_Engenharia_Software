e547a11728a2b2eb65b51ee4487112cf5e226635
mateusvmv <mateusv251@gmail.com>
2025-03-31 14:16:28 -0300

    arruma loop nas migrações

--- src/migrations.ts
+++ src/migrations.ts
@@ -8,16 +8,22 @@ const db = new Database('db/sqlite');
 db.pragma('journal_mode = WAL');
 
 const version = db.pragma('user_version', { simple: true });
-db.exec('BEGIN');
-while(1) switch(version) {
-    case 0:
-        // Aqui entra uma migração. Ao fim da migração, atualizar a versão:
-        // db.pragma('user_version = 39400698');
-        // Usar um número aleatório para evitar conflitos entre branches
-        // Assim, as migrações formam uma árvore e todas as branches convergem
-        break;
-    default:
-        console.error('A versão do banco de dados não é conhecida:', version);
-        exit(1);
+
+function migrate() {
+    while(1) switch(version) {
+        case 0:
+            // Aqui entra uma migração. Ao fim da migração, atualizar a versão:
+            // db.pragma('user_version = 39400698');
+            // Usar um número aleatório para evitar conflitos entre branches
+            // Assim, as migrações formam uma árvore e todas as branches convergem
+            // A versão final retorna a função, as outras dão continue
+            return;
+        default:
+            console.error('A versão do banco de dados não é conhecida:', version);
+            exit(1);
+    }
 }
+
+db.exec('BEGIN');
+migrate();
 db.exec('COMMIT');
